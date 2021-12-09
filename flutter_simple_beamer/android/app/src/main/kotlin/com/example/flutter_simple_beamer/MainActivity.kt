8edc6cb3499dda6baf4ebd83d4c51f85273fe7af
Fikri Prayoga <fikri@extrainteger.com>
2021-12-10 05:55:47 +0700

    Project init

---
+++ flutter_simple_beamer/android/app/src/main/kotlin/com/example/flutter_simple_beamer/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_simple_beamer
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ flutter_simple_beamer/lib/main.dart
@@ -0,0 +1,140 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ScreenUtilInit(
+      designSize: const Size(1920, 1080),
+      builder: () => MaterialApp(
+        title: 'Flutter Simple Beamer',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: const MyHomePage(),
+      ),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Flutter Simple Beamer'),
+      ),
+      body: _body(),
+    );
+  }
+
+  Widget _body() {
+    return Row(
+      children: [
+        _menuSection(),
+        _content(),
+      ],
+    );
+  }
+
+  Widget _menuSection() {
+    return Container(
+      width: 300.w,
+      height: double.infinity,
+      color: Colors.blue[700],
+      child: Column(
+        children: [
+          _menuHeader(),
+          SizedBox(height: 50.h),
+          InkWell(
+            onTap: () {},
+            child: _menuItem('Menu 0'),
+          ),
+          InkWell(
+            onTap: () {},
+            child: _menuItem('Menu 1'),
+          ),
+          InkWell(
+            onTap: () {},
+            child: _menuItem('Menu 2'),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _menuHeader() {
+    return Container(
+      width: double.infinity,
+      height: 300.h,
+      color: Colors.blue,
+      child: const Center(
+          child: Text(
+        'Header',
+        style: TextStyle(
+          fontWeight: FontWeight.bold,
+          color: Colors.white,
+        ),
+      )),
+    );
+  }
+
+  Widget _menuItem(String menuName) {
+    return Container(
+      width: double.infinity,
+      height: 50.h,
+      padding: EdgeInsets.symmetric(horizontal: 20.w),
+      child: Align(
+        alignment: Alignment.centerLeft,
+        child: Text(
+          menuName,
+          style: const TextStyle(
+            fontWeight: FontWeight.bold,
+            color: Colors.white,
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _content() {
+    return Expanded(
+      child: Center(
+        child: _board(),
+      ),
+    );
+  }
+
+  Widget _board() {
+    return Card(
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.sp)),
+      elevation: 8.sp,
+      child: SizedBox(
+        width: 300.sp,
+        height: 300.sp,
+        child: Center(
+          child: Text(
+            'Widget 0',
+            style: TextStyle(
+              color: Colors.blue,
+              fontSize: 25.sp,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
