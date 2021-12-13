c875b6838cd49b609575b4c5956fd433311aea89
Fikri Prayoga <fikri@extrainteger.com>
2021-12-13 19:24:37 +0700

    Project done

---
+++ flutter_simple_beamer/lib/beamer_location.dart
@@ -0,0 +1,35 @@
+import 'package:beamer/beamer.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_simple_beamer/view_home.dart';
+
+class HomeLocation extends BeamLocation<BeamState> {
+  HomeLocation(RouteInformation routeInformation) : super(routeInformation);
+  @override
+  List<String> get pathPatterns => [
+        '/',
+        '/:menuIndex',
+      ];
+
+  @override
+  List<BeamPage> buildPages(BuildContext context, BeamState state) => [
+        BeamPage(
+          key: ValueKey('home-${state.uri}'),
+          title: 'Home',
+          child: ViewHome(
+            menuIndex: (context.currentBeamLocation.state as BeamState)
+                    .pathParameters['menuIndex'] ??
+                '',
+          ),
+        ),
+        // if (state.pathParameters.containsKey('menuIndex'))
+        //   BeamPage(
+        //     key: ValueKey('book-${state.pathParameters['menuIndex']}'),
+        //     title: 'title',
+        //     child: ViewContent(
+        //       menuIndex: (context.currentBeamLocation.state as BeamState)
+        //               .pathParameters['menuIndex'] ??
+        //           '',
+        //     ),
+        //   ),
+      ];
+}
